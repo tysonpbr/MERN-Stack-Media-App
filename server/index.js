7e1cf19ab00af93b1d3c7c93dfe21f6d24924c25
Tyson Brown <90238566+tysonpbr@users.noreply.github.com>
2023-01-15 10:35:47 -0800

    added all posts funtionality

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
@@ -13,9 +14,11 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://user:user123@cluster0.bupcoyp.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
+const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
